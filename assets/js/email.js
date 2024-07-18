b1679354d2f5eed5235241a3cee7774daedcf96b
Shieliang <shieliang22@gmail.com>
2024-07-18 12:00:12 +0800

    Implement Emailjs with regression test

--- assets/js/email.js
+++ assets/js/email.js
@@ -6,7 +6,14 @@ function sendmail(){
         email : document.getElementById("email").value,
         subject : document.getElementById("subject").value,
         message : document.getElementById("message").value
-    }
+    };
 
-    emailjs.send("service_8d9n0nk","template_fpgbyig",parms).then(alert("Email Sent"))
+    emailjs.send("service_8d9n0nk", "template_fpgbyig", parms)
+        .then(function(response) {
+            alert("Email Sent Successfully!");
+            console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+            alert("Failed to Send Email. Please try again.");
+            console.log('FAILED...', error);
+        });
 }

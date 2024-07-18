345367814950e682b4a20a1cae8563560ae2ce8e
Shieliang <shieliang22@gmail.com>
2024-07-18 11:38:49 +0800

    Implement email

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', function(){
-    emailjs.init("_RJTPq-VrE4_rZUKT");
-});
+function sendmail(){
+
+    let parms={
+
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value
+    }
+
+    emailjs.send("service_8d9n0nk","template_fpgbyig",parms).then(alert("Email Sent"))
+}
